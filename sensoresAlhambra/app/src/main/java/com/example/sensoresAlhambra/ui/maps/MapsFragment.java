8136636d7a5773e730e88231268e23fdffc5ef2a
Jose26398 <jsg26398@gmail.com>
2019-10-24 11:21:31 +0200

    360 image viewer

--- sensoresAlhambra/app/src/main/java/com/example/sensoresAlhambra/ui/maps/MapsFragment.java
+++ sensoresAlhambra/app/src/main/java/com/example/sensoresAlhambra/ui/maps/MapsFragment.java
@@ -1,5 +1,7 @@
 package com.example.sensoresAlhambra.ui.maps;
 
+import android.content.res.AssetManager;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,16 +12,55 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.sensoresAlhambra.R;
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+
+import java.io.InputStream;
 
 public class MapsFragment extends Fragment {
 
-    private MapsViewModel galleryViewModel;
+    private MapsViewModel  mapsViewModel;
+    private VrPanoramaView mVRPanoramaView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
+        mapsViewModel =
                 ViewModelProviders.of(this).get(MapsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_maps, container, false);
+        mVRPanoramaView = (VrPanoramaView) root.findViewById(R.id.vrPanoramaView);
+        loadPhotoSphere("image.jpg");
         return root;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mVRPanoramaView.pauseRendering();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mVRPanoramaView.resumeRendering();
+    }
+
+    @Override
+    public void onDestroy() {
+        mVRPanoramaView.shutdown();
+        super.onDestroy();
+    }
+
+    private void loadPhotoSphere(String foto) {
+        VrPanoramaView.Options options = new VrPanoramaView.Options();
+        InputStream inputStream = null;
+        AssetManager assetManager = this.getContext().getAssets();
+
+        try {
+            inputStream = assetManager.open(foto);
+            options.inputType = VrPanoramaView.Options.TYPE_MONO;
+            mVRPanoramaView.loadImageFromBitmap(BitmapFactory.decodeStream(inputStream), options);
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
